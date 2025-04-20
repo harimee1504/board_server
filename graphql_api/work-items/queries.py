f7c125d8b6880bc8c62f78be6ee9c61c7ababee0
harikrishnan <harimee1504@gmail.com>
2025-04-20 20:05:06 +0530

    sprint board changes

--- graphql_api/work-items/queries.py
+++ graphql_api/work-items/queries.py
@@ -1,6 +1,6 @@
 from ariadne import QueryType
 from utils.utils import has_permission
-from services.work_items import GetWorkItems
+from services.work_items import GetWorkItems, GetActiveUserStories
 
 query = QueryType()
 
@@ -10,6 +10,10 @@ def resolve_get_work_items(*args):
     obj = GetWorkItems()
     return obj.get()
 
-    
+@query.field("getActiveUserStories")
+@has_permission("org:board:read")
+def resolve_get_active_user_stories(*args):
+    obj = GetActiveUserStories()
+    return obj.get()
 
 resolvers = [query]
--- services/work_items/__init__.py
+++ services/work_items/__init__.py
@@ -3,6 +3,7 @@ from .update_work_item import UpdateWorkItem
 from .delete_work_item import DeleteWorkItem
 from .get_work_items import GetWorkItems
 from .update_work_item_state import UpdateWorkItemState
+from .get_active_user_stories import GetActiveUserStories
 
 
-__all__ = ["CreateWorkItem", "UpdateWorkItem", "DeleteWorkItem", "GetWorkItems", "UpdateWorkItemState"]
+__all__ = ["CreateWorkItem", "UpdateWorkItem", "DeleteWorkItem", "GetWorkItems", "UpdateWorkItemState", "GetActiveUserStories"]
